7b7bd6d658359ccc57755bc0e941d5ab18a60b3f
Wayne Lam <w.p.lam107@gmail.com>
2022-02-10 17:33:13 -0500

    many changes

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,3 @@
-from unicodedata import name
-from click import style
 import dash
 from dash import dcc
 from dash import html
@@ -13,12 +11,13 @@ import pandas as pd
 import shapely.geometry
 import numpy as np
 import datetime
+import json
 
 import pickle
 
 NYC_LAT_LONG = {'lon': -74.0060, 'lat': 40.7128}
-FILM_PERMITS = 'film_df.p'
-ZIP_CODES = 'zip_codes.p'
+FILM_PERMITS = './data/film_df.p'
+ZIP_CODES = './data/zip_codes.p'
 BORO_DICT = {
     'New York': 'Manhattan',
     'Kings': 'Brooklyn',
@@ -26,6 +25,8 @@ BORO_DICT = {
     'Bronx': 'Bronx',
     'Richmond': 'Staten Island'
 }
+MAP_HEIGHT = 800
+MAP_WIDTH = 1500
 
 external_stylesheets = ['https://taniarascia.github.io/primitive/css/main.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -39,6 +40,10 @@ with open(ZIP_CODES, 'rb') as f:
 df['startdate'] = pd.to_datetime(df['startdate']).dt.date
 df['enddate'] = pd.to_datetime(df['enddate']).dt.date
 df['enteredon'] = pd.to_datetime(df['enteredon']).dt.date
+df.rename(columns={'id': 'id_'}, inplace=True)
+df = df[[
+    'id_', 'zipcode', 'startdate', 'enddate', 'category', 'subcategory', 'origin', 'main_st', 'cross_st_1', 'cross_st_2', 'geometry'
+]]
 
 default_map_fig = go.Figure(go.Scattermapbox(
     lat=[None],
@@ -51,16 +56,14 @@ default_map_fig.update_layout(
         'center': NYC_LAT_LONG,
         'zoom': 10
     },
-    height=900,
-    width=1000
+    height=MAP_HEIGHT,
+    width=MAP_WIDTH
 )
 
 app.layout = html.Div(children=[
     html.H1(children='NYC Film Shoots'),
 
-    html.Div(children='''
-        Map of blocks shutdown for NYC film shoots
-    '''),
+    dcc.Store(id='filtered-data'),
 
     html.Div([
         dcc.DatePickerRange(
@@ -75,22 +78,21 @@ app.layout = html.Div(children=[
         id='container-figures',
         type='default',
         children=[
-            dcc.Graph(id='film-map')
+            dcc.Graph(id='film-map', figure=default_map_fig)
         ]
     )
 ])
 
 @app.callback(
-    Output('film-map', 'figure'),
+    Output('filtered-data', 'data'),
     Input('date-picker', 'start_date'),
     Input('date-picker', 'end_date')
 )
-def fig_by_date(startdate: datetime.date, enddate: datetime.date):
-
+def pick_dates(startdate: datetime.date, enddate: datetime.date):
     if (startdate == None) or (enddate == None):
-        return default_map_fig
+        return None
     if startdate > enddate:
-        return default_map_fig
+        return None
 
     startdate = datetime.datetime.strptime(startdate, '%Y-%m-%d').date()
     enddate = datetime.datetime.strptime(enddate, '%Y-%m-%d').date()
@@ -99,8 +101,28 @@ def fig_by_date(startdate: datetime.date, enddate: datetime.date):
     filtered_df['parking_held'] = filtered_df.apply(
         lambda x: x['main_st'].upper() + ' between ' + x['cross_st_1'].upper() + ' and ' + x['cross_st_2'].upper(), axis=1
     )
+    filtered_df['startdate'] = pd.to_datetime(filtered_df['startdate']).dt.strftime('%Y-%m-%d')
+    filtered_df['enddate'] = pd.to_datetime(filtered_df['enddate']).dt.strftime('%Y-%m-%d')
 
-    temp = pd.DataFrame(filtered_df['zipcode'].to_list(), index=filtered_df['id']).stack().reset_index()
+    return json.dumps(filtered_df.__geo_interface__)
+
+@app.callback(
+    Output('film-map', 'figure'),
+    Input('filtered-data', 'data')
+)
+def fig_by_date(filtered_json):
+    if filtered_json == None:
+        return default_map_fig
+
+    j = json.loads(filtered_json)
+    filtered_df = gpd.GeoDataFrame.from_features(j)
+    filtered_df['startdate'] = pd.to_datetime(filtered_df['startdate']).dt.date
+    filtered_df['enddate'] = pd.to_datetime(filtered_df['enddate']).dt.date
+
+    startdate = filtered_df['startdate'].min()
+    enddate = filtered_df['enddate'].max()
+
+    temp = pd.DataFrame(filtered_df['zipcode'].to_list(), index=filtered_df['id_']).stack().reset_index()
     temp.drop(columns='level_1', inplace=True)
     temp.columns = ['permit_id', 'zip_code']
     temp = temp.groupby('zip_code')['permit_id'].unique().reset_index()
@@ -125,7 +147,7 @@ def fig_by_date(startdate: datetime.date, enddate: datetime.date):
 
     data = zip(
         filtered_df['geometry'],
-        filtered_df['id'],
+        filtered_df['id_'],
         filtered_df['category'],
         filtered_df['subcategory'],
         filtered_df['origin'],
@@ -185,30 +207,19 @@ def fig_by_date(startdate: datetime.date, enddate: datetime.date):
         locations=counts.index,
         color='permit_count',
         custom_data=['permit_count'],
-        title=f'Data Range: {startdate} - {enddate}',
+        title=f'Data Range: {startdate} through {enddate}',
         opacity=0.1,
         mapbox_style='carto-positron',
         center=NYC_LAT_LONG,
         zoom=10,
-        height=800,
-        width=800
+        height=MAP_HEIGHT,
+        width=MAP_WIDTH
     )
     fig.update_traces(
         hovertemplate='<b>Zip Code:</b> %{location}<br><b>Permit Count:</b> %{customdata[0]}'
     )
     
     fig.add_trace(scatter)
-
-    # fig.update_layout(
-    #     title=f'Data Range: {startdate} - {enddate}',
-    #     mapbox={
-    #         'style': 'carto-positron',
-    #         'center': NYC_LAT_LONG,
-    #         'zoom': 10
-    #     },
-    #     height=900,
-    #     width=1000
-    # )
 
     return fig
 
