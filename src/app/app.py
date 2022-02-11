e180d8a75a8dd85329fee8816e3510478dd0851f
Wayne Lam <w.p.lam107@gmail.com>
2022-02-11 16:50:37 -0500

    added top zipcode graph

--- src/app/app.py
+++ src/app/app.py
@@ -1,21 +1,13 @@
-import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
-
-import plotly.express as px
-import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output
 
 import geopandas as gpd
 import pandas as pd
-import shapely.geometry
-import numpy as np
 import datetime
 import json
-
 import pickle
 
-NYC_LAT_LONG = {'lon': -74.0060, 'lat': 40.7128}
+import graphing_callbacks
+
 FILM_PERMITS = './data/film_df.p'
 ZIP_CODES = './data/zip_codes.p'
 BORO_DICT = {
@@ -25,11 +17,10 @@ BORO_DICT = {
     'Bronx': 'Bronx',
     'Richmond': 'Staten Island'
 }
-MAP_HEIGHT = 800
-MAP_WIDTH = 1500
 
 external_stylesheets = ['https://taniarascia.github.io/primitive/css/main.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+server = app.server
 
 with open(FILM_PERMITS, 'rb') as f:
     df = gpd.GeoDataFrame(pickle.load(f))
@@ -41,58 +32,73 @@ df['startdate'] = pd.to_datetime(df['startdate']).dt.date
 df['enddate'] = pd.to_datetime(df['enddate']).dt.date
 df['enteredon'] = pd.to_datetime(df['enteredon']).dt.date
 df.rename(columns={'id': 'id_'}, inplace=True)
-df = df[[
+df = gpd.GeoDataFrame(df[[
     'id_', 'zipcode', 'startdate', 'enddate', 'category', 'subcategory', 'origin', 'main_st', 'cross_st_1', 'cross_st_2', 'geometry'
-]]
-
-default_map_fig = go.Figure(go.Scattermapbox(
-    lat=[None],
-    lon=[None],
-    name='Enter date range',
-))
-default_map_fig.update_layout(
-    mapbox={
-        'style': 'carto-positron',
-        'center': NYC_LAT_LONG,
-        'zoom': 10
-    },
-    height=MAP_HEIGHT,
-    width=MAP_WIDTH
-)
+]])
 
 app.layout = html.Div(children=[
-    html.H1(children='NYC Film Shoots'),
+    html.H1(children='NYC Film Shoots', style={'textAlign': 'center'}),
 
-    dcc.Store(id='filtered-data'),
+    dcc.Store(id='filtered-shoots'),
+    dcc.Store(id='zipcode-shoots'),
 
-    html.Div([
-        dcc.DatePickerRange(
-            id='date-picker',
-            min_date_allowed=df['startdate'].min(),
-            max_date_allowed=df['enddate'].max(),
-            initial_visible_month=df['startdate'].min()
-        )
-    ]),
+    html.Div(
+        children=[
+            'Select Date',
+            html.Br(),
+            dcc.DatePickerRange(
+                id='date-picker',
+                min_date_allowed=df['startdate'].min(),
+                max_date_allowed=df['enddate'].max(),
+                initial_visible_month=df['startdate'].min()
+            )
+        ],
+        style={'textAlign': 'center'}
+    ),
 
     dcc.Loading(
-        id='container-figures',
         type='default',
-        children=[
-            dcc.Graph(id='film-map', figure=default_map_fig)
-        ]
+        children=html.Div(
+            id='container-figures',
+            className='row',
+            children=html.Div(
+                children=[
+                    html.Div(
+                        id='container-map',
+                        children=html.Div(dcc.Graph(
+                            id='film-map',
+                            style={'height': '80vh', 'width': '80vh'},
+                            config={'displayModeBar': False}
+                        )),
+                        className='six columns',
+                        style={'display': 'inline-block'}
+                    ),
+                    html.Div(
+                        id='container-bar',
+                        children=html.Div(dcc.Graph(
+                            id='zipcode-bar',
+                            style={'height': '80vh', 'width': '80vh'},
+                            config={'displayModeBar': False}
+                        )),
+                        className='six columns',
+                        style={'display': 'inline-block'}
+                    )
+                ]
+            ),
+            style={'justify-content': 'center', 'align-items': 'center', 'display': 'flex'}
+        )
     )
 ])
 
 @app.callback(
-    Output('filtered-data', 'data'),
-    Input('date-picker', 'start_date'),
-    Input('date-picker', 'end_date')
+    [Output('filtered-shoots', 'data'), Output('zipcode-shoots', 'data')],
+    [Input('date-picker', 'start_date'), Input('date-picker', 'end_date')]
 )
 def pick_dates(startdate: datetime.date, enddate: datetime.date):
     if (startdate == None) or (enddate == None):
-        return None
+        return (None, None)
     if startdate > enddate:
-        return None
+        return (None, None)
 
     startdate = datetime.datetime.strptime(startdate, '%Y-%m-%d').date()
     enddate = datetime.datetime.strptime(enddate, '%Y-%m-%d').date()
@@ -101,26 +107,6 @@ def pick_dates(startdate: datetime.date, enddate: datetime.date):
     filtered_df['parking_held'] = filtered_df.apply(
         lambda x: x['main_st'].upper() + ' between ' + x['cross_st_1'].upper() + ' and ' + x['cross_st_2'].upper(), axis=1
     )
-    filtered_df['startdate'] = pd.to_datetime(filtered_df['startdate']).dt.strftime('%Y-%m-%d')
-    filtered_df['enddate'] = pd.to_datetime(filtered_df['enddate']).dt.strftime('%Y-%m-%d')
-
-    return json.dumps(filtered_df.__geo_interface__)
-
-@app.callback(
-    Output('film-map', 'figure'),
-    Input('filtered-data', 'data')
-)
-def fig_by_date(filtered_json):
-    if filtered_json == None:
-        return default_map_fig
-
-    j = json.loads(filtered_json)
-    filtered_df = gpd.GeoDataFrame.from_features(j)
-    filtered_df['startdate'] = pd.to_datetime(filtered_df['startdate']).dt.date
-    filtered_df['enddate'] = pd.to_datetime(filtered_df['enddate']).dt.date
-
-    startdate = filtered_df['startdate'].min()
-    enddate = filtered_df['enddate'].max()
 
     temp = pd.DataFrame(filtered_df['zipcode'].to_list(), index=filtered_df['id_']).stack().reset_index()
     temp.drop(columns='level_1', inplace=True)
@@ -134,94 +120,13 @@ def fig_by_date(filtered_json):
     counts['permit_count'] = counts['permit_count'].fillna(0)
     counts['permit_count'] = counts['permit_count'].astype('int')
     counts.set_index('zipcode', inplace=True)
+    counts['zipcode'] = counts.index
 
-    lats = []
-    lons = []
-    ids = []
-    cats = []
-    subcats = []
-    countries = []
-    sdates = []
-    edates = []
-    phs = []
+    filtered_df['startdate'] = pd.to_datetime(filtered_df['startdate']).dt.strftime('%Y-%m-%d')
+    filtered_df['enddate'] = pd.to_datetime(filtered_df['enddate']).dt.strftime('%Y-%m-%d')
 
-    data = zip(
-        filtered_df['geometry'],
-        filtered_df['id_'],
-        filtered_df['category'],
-        filtered_df['subcategory'],
-        filtered_df['origin'],
-        filtered_df['startdate'],
-        filtered_df['enddate'],
-        filtered_df['parking_held']
-    )
-
-    for feature, id_, cat, subcat, country, sdate, edate, ph in data:
-        if isinstance(feature, shapely.geometry.linestring.LineString):
-            linestrings = [feature]
-        elif isinstance(feature, shapely.geometry.multilinestring.MultiLineString):
-            linestrings = feature.geoms
-        else:
-            continue
-        for linestring in linestrings:
-            x, y = linestring.xy
-            lats = np.append(lats, y)
-            lons = np.append(lons, x)
-            ids = np.append(ids, [id_]*len(y))
-            cats = np.append(cats, [cat]*len(y))
-            subcats = np.append(subcats, [subcat]*len(y))
-            countries = np.append(countries, [country]*len(y))
-            sdates = np.append(sdates, [sdate]*len(y))
-            edates = np.append(edates, [edate]*len(y))
-            phs = np.append(phs, [ph]*len(y))
-            lats = np.append(lats, None)
-            lons = np.append(lons, None)
-            ids = np.append(ids, None)
-            cats = np.append(cats, None)
-            subcats = np.append(subcats, None)
-            countries = np.append(countries, None)
-            sdates = np.append(sdates, None)
-            edates = np.append(edates, None)
-            phs = np.append(phs, None)
-
-    scatter = go.Scattermapbox(
-        mode='lines',
-        lat=lats,
-        lon=lons,
-        customdata=np.stack((ids, cats, subcats, countries, sdates, edates, phs), axis=-1),
-        hovertemplate='<br>'.join([
-            '<b>Permit ID:</b> %{customdata[0]}',
-            '<b>Category:</b> %{customdata[1]}',
-            '<b>Subcategory:</b> %{customdata[2]}',
-            '<b>Country:</b> %{customdata[3]}',
-            '<b>Start Date:</b> %{customdata[4]}',
-            '<b>End Date:</b> %{customdata[5]}',
-            '<b>Parking Held:</b> %{customdata[6]}'
-        ]),
-        name=''
-    )
-
-    fig = px.choropleth_mapbox(
-        counts,
-        geojson=counts['geometry'],
-        locations=counts.index,
-        color='permit_count',
-        custom_data=['permit_count'],
-        title=f'Data Range: {startdate} through {enddate}',
-        opacity=0.1,
-        mapbox_style='carto-positron',
-        center=NYC_LAT_LONG,
-        zoom=10,
-        height=MAP_HEIGHT,
-        width=MAP_WIDTH
-    )
-    fig.update_traces(
-        hovertemplate='<b>Zip Code:</b> %{location}<br><b>Permit Count:</b> %{customdata[0]}'
-    )
-    
-    fig.add_trace(scatter)
-
-    return fig
+    # __geo_interface__ is GeoJSON as str
+    return json.dumps(filtered_df.__geo_interface__), json.dumps(counts.__geo_interface__)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ src/app/graphing_callbacks.py
@@ -0,0 +1,197 @@
+from dash import Input, Output
+from dash import callback
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+import geopandas as gpd
+import pandas as pd
+import shapely.geometry
+import numpy as np
+import json
+import pickle
+
+
+NYC_LAT_LONG = {'lon': -74.0060, 'lat': 40.7128}
+
+default_map_fig = go.Figure(go.Scattermapbox(
+    lat=[None],
+    lon=[None]
+))
+default_map_fig.update_layout(
+    mapbox={
+        'style': 'carto-positron',
+        'center': NYC_LAT_LONG,
+        'zoom': 10
+    },
+    title={
+        'text': 'Map of Blocks with Film Shoots',
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    }
+)
+
+default_bar_fig = go.Figure(px.bar())
+default_bar_fig.update_layout(
+    title={
+        'text': 'Top Zip Codes',
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'
+    },
+    margin={
+        't': 100
+    },
+    yaxis_title='Permit Counts',
+    xaxis_title='Zip Code'
+)
+default_bar_fig.update_yaxes(showticklabels=False)
+default_bar_fig.update_xaxes(showticklabels=False)
+
+@callback(
+    Output('film-map', 'figure'),
+    Input('filtered-shoots', 'data'),
+    Input('zipcode-shoots', 'data')
+)
+def fig_by_date(filtered_json, zipcode_json):
+    if filtered_json == None:
+        return default_map_fig
+
+    j = json.loads(filtered_json)
+    filtered_df = gpd.GeoDataFrame.from_features(j)
+    filtered_df['startdate'] = pd.to_datetime(filtered_df['startdate']).dt.date
+    filtered_df['enddate'] = pd.to_datetime(filtered_df['enddate']).dt.date
+
+    c = json.loads(zipcode_json)
+    counts = gpd.GeoDataFrame.from_features(c)
+
+    lats = []
+    lons = []
+    ids = []
+    cats = []
+    subcats = []
+    countries = []
+    sdates = []
+    edates = []
+    phs = []
+
+    data = zip(
+        filtered_df['geometry'],
+        filtered_df['id_'],
+        filtered_df['category'],
+        filtered_df['subcategory'],
+        filtered_df['origin'],
+        filtered_df['startdate'],
+        filtered_df['enddate'],
+        filtered_df['parking_held']
+    )
+
+    for feature, id_, cat, subcat, country, sdate, edate, ph in data:
+        if isinstance(feature, shapely.geometry.linestring.LineString):
+            linestrings = [feature]
+        elif isinstance(feature, shapely.geometry.multilinestring.MultiLineString):
+            linestrings = feature.geoms
+        else:
+            continue
+        for linestring in linestrings:
+            x, y = linestring.xy
+            lats = np.append(lats, y)
+            lons = np.append(lons, x)
+            ids = np.append(ids, [id_]*len(y))
+            cats = np.append(cats, [cat]*len(y))
+            subcats = np.append(subcats, [subcat]*len(y))
+            countries = np.append(countries, [country]*len(y))
+            sdates = np.append(sdates, [sdate]*len(y))
+            edates = np.append(edates, [edate]*len(y))
+            phs = np.append(phs, [ph]*len(y))
+            lats = np.append(lats, None)
+            lons = np.append(lons, None)
+            ids = np.append(ids, None)
+            cats = np.append(cats, None)
+            subcats = np.append(subcats, None)
+            countries = np.append(countries, None)
+            sdates = np.append(sdates, None)
+            edates = np.append(edates, None)
+            phs = np.append(phs, None)
+
+    scatter = go.Scattermapbox(
+        mode='lines',
+        lat=lats,
+        lon=lons,
+        customdata=np.stack((ids, cats, subcats, countries, sdates, edates, phs), axis=-1),
+        hovertemplate='<br>'.join([
+            '<b>Permit ID:</b> %{customdata[0]}',
+            '<b>Category:</b> %{customdata[1]}',
+            '<b>Subcategory:</b> %{customdata[2]}',
+            '<b>Country:</b> %{customdata[3]}',
+            '<b>Start Date:</b> %{customdata[4]}',
+            '<b>End Date:</b> %{customdata[5]}',
+            '<b>Parking Held:</b> %{customdata[6]}'
+        ]),
+        name=''
+    )
+
+    fig = go.Figure(px.choropleth_mapbox(
+        counts,
+        geojson=counts['geometry'],
+        locations=counts.index,
+        color='permit_count',
+        custom_data=['zipcode', 'permit_count'],
+        title='Map of Blocks with Film Shoots',
+        opacity=0.1,
+        mapbox_style='carto-positron',
+        center=NYC_LAT_LONG,
+        zoom=10
+    ))
+    fig.update_traces(
+        hovertemplate='<b>Zip Code:</b> %{customdata[0]}<br><b>Permit Count:</b> %{customdata[1]}'
+    )
+    fig.add_trace(scatter)
+    fig.update_layout(
+        coloraxis_showscale=False,
+        title={
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        }
+    )
+
+    return fig
+
+@callback(
+    Output('zipcode-bar', 'figure'),
+    Input('zipcode-shoots', 'data')
+)
+def top_ten_zc(zipcode_json):
+    if zipcode_json == None:
+        return default_bar_fig
+
+    c = json.loads(zipcode_json)
+    counts = gpd.GeoDataFrame.from_features(c)
+    counts.sort_values('permit_count', ascending=False, inplace=True)
+    counts = counts.iloc[:10]
+
+    fig = go.Figure(px.bar(data_frame=counts, x='zipcode', y='permit_count', labels={'permit_count': 'Permit Count', 'zipcode': 'Zip Code'}))
+    fig.update_layout(
+        title={
+            'text': 'Top Zip Codes',
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        margin={
+            't': 100
+        },
+        yaxis_title='Permit Counts',
+        xaxis_title='Zip Code'
+    )
+    fig.update_traces(
+        hovertemplate='<b>Zip Code:</b> %{x}<br><b>Permit Count:</b> %{y}'
+    )
+
+    return fig
